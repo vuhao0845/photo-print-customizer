d24810e022365881cfff1b91230e0b866389540a
vuhao0845 <vuhao0845@gmail.com>
2025-10-21 16:02:23 +0700

    Update _app.js

--- photo-print-customizer-repo/pages/_app.js
+++ photo-print-customizer-repo/pages/_app.js
@@ -1,4 +1,5 @@
-import '@/styles/globals.css';
+import '../styles/globals.css'; // Đảm bảo rằng đường dẫn này chính xác
+
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
